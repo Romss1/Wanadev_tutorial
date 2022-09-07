3d3eb8ef5cdabc640e83541deb1442ed31aa5535
lreus <ludovic.reus@free.fr>
2022-09-07 16:25:39 +0200

    chore: implement postgresql driver into php-fpm container
    - add script from [ondrej repository](https://packages.sury.org/php) to install php repository
    - Create new container from bitnami/php-fpm with pdo-pgsql
    - update docker-compose configuration
    - update make install command to build docker container

---
+++ docker-config/php/install_postgre_driver.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# To add this repository please do:
+
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+${SUDO} apt-get update
+${SUDO} apt-get -y install apt-transport-https lsb-release ca-certificates curl
+${SUDO} curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
+${SUDO} sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+${SUDO} apt-get update
+${SUDO} apt-get -y install php8.1-pgsql
